6261573fd797e6c190266ff847b02afc999fe639
Mike Murray <hellomikemurray@gmail.com>
2019-07-22 15:32:27 -0700

    feat: add mui theme provider to wrapper
    Signed-off-by: Mike Murray <hellomikemurray@gmail.com>

--- styleguide/src/components/Wrapper.js
+++ styleguide/src/components/Wrapper.js
@@ -1,17 +1,22 @@
 import React from "react";
 import DefaultWrapper from "react-styleguidist/lib/client/rsg-components/Wrapper/Wrapper";
 import { ComponentsProvider } from "@reactioncommerce/components-context";
+import { MuiThemeProvider } from "@material-ui/core/styles";
 import appComponents from "../appComponents";
+import { defaultTheme } from "../../../package/src";
 
 /**
  * @name Wrapper
  * @summary wraps example components with ComponentsProvider
+ * @param {Object} props Component props
  * @returns {Object} with wrapped component(s)
  */
-function Wrapper() {
+function Wrapper(props) {
   return (
     <ComponentsProvider value={appComponents}>
-      <DefaultWrapper {...this.props} />
+      <MuiThemeProvider muiTheme={defaultTheme}>
+        <DefaultWrapper {...props} />
+      </MuiThemeProvider>
     </ComponentsProvider>
   );
 }
