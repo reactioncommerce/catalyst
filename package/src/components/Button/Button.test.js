47673287fe08c6dc225b25538a385077389ea667
Machiko Yasuda <machiko@reactioncommerce.com>
2019-07-23 21:15:23 -0700

    test(button): attempt to fix test
    Signed-off-by: Machiko Yasuda <machiko@reactioncommerce.com>

--- package/src/components/Button/Button.test.js
+++ package/src/components/Button/Button.test.js
@@ -1,10 +1,14 @@
 import React from "react";
 import renderer from "react-test-renderer";
+import { MuiThemeProvider } from "@material-ui/core/styles";
+import { defaultTheme } from "../../theme/defaultTheme";
 import mockComponents from "../../tests/mockComponents";
 import Button from "./Button";
 
 test("basic snapshot", () => {
-  const component = renderer.create(<Button components={mockComponents} title="title" className="a b">Submit</Button>);
+  const component = renderer.create(<MuiThemeProvider theme={defaultTheme}>
+    <Button components={mockComponents} title="title" className="a b">Submit</Button>
+  </MuiThemeProvider>);
 
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
