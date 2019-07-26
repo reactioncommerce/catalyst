0537b62bf0ffda54c133cebd38cd3c80a76d68e8
Machiko Yasuda <machiko@reactioncommerce.com>
2019-07-26 12:14:04 -0700

    perf(imports): use tree-shaking-friendly imports
    Signed-off-by: Machiko Yasuda <machiko@reactioncommerce.com>

--- package/src/components/Button/Button.js
+++ package/src/components/Button/Button.js
@@ -1,8 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import CircularProgress from "@material-ui/core/CircularProgress";
-import MuiButton from "@material-ui/core/Button";
-import makeStyles from "@material-ui/core/styles/makeStyles";
+import { MuiButton, CircularProgress, makeStyles } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   buttonProgress: {
