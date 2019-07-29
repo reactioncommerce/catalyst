ace3cb4ee5b4191701bddaf2fe8973b1aaa58d8f
Christopher Shepherd <christopher@reactioncommerce.com>
2019-07-29 14:50:34 -0400

    test: Add test for Chip
    Signed-off-by: Christopher Shepherd <christopher@reactioncommerce.com>

--- package/src/components/Chip/Chip.test.js
+++ package/src/components/Chip/Chip.test.js
@@ -1,11 +1,13 @@
 import React from "react";
-import renderer from "react-test-renderer";
+import { render } from "../../tests/index.js";
 import Chip from "./Chip";
 
-test("basic snapshot", () => {
-  const component = renderer.create(<Chip title="title" className="a b"/>);
-
-  const tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
+test("basic snapshot - only default props", () => {
+  const { asFragment } = render(<Chip />);
+  expect(asFragment()).toMatchSnapshot();
 });
 
+test("error chip snapshot", () => {
+  const { asFragment } = render(<Chip color="error" variant="contained" />);
+  expect(asFragment()).toMatchSnapshot();
+});
