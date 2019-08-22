7a6ad1e7ceea050f87f2db39cbae3b4bc13e1a04
Mike Murray <hellomikemurray@gmail.com>
2019-08-22 14:07:00 -0700

    test: add ActionMenu tests
    Signed-off-by: Mike Murray <hellomikemurray@gmail.com>

---
+++ package/src/components/ActionMenu/ActionMenu.test.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { fireEvent, render, waitForElement } from "../../tests/index.js";
+import ActionMenu from "./ActionMenu";
+
+const options = [{
+  label: "Add tags to products"
+}, {
+  label: "Remove tags from products",
+  isDestructive: true
+}, {
+  label: "Remove all tags",
+  isDisabled: true,
+  isDestructive: true
+}];
+
+test("basic snapshot - only default props", () => {
+  const { asFragment } = render(<ActionMenu options={options}>Actions</ActionMenu>);
+  expect(asFragment()).toMatchSnapshot();
+});
+
+test("select an option", async () => {
+  const onSelect = jest.fn();
+  const { asFragment, getByText } = render(<ActionMenu onSelect={onSelect} options={options}>Actions</ActionMenu>);
+  fireEvent.click(getByText("Actions"));
+  const removeButton = await waitForElement(() => getByText("Remove tags from products"));
+  fireEvent.click(removeButton);
+  expect(onSelect).toHaveBeenCalled();
+  expect(asFragment()).toMatchSnapshot();
+});
