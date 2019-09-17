f3b852b68081d6dd7e18402a1e609ccbd1747e60
Mike Murray <hellomikemurray@gmail.com>
2019-09-17 15:19:27 -0700

    feat: add pagination controls
    Signed-off-by: Mike Murray <hellomikemurray@gmail.com>

--- package/src/components/DataTable/DataTable.js
+++ package/src/components/DataTable/DataTable.js
@@ -1,39 +1,46 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { useTable, usePagination } from "react-table";
 import {
+  Box,
   Table,
-  TableHead,
   TableBody,
   TableCell,
+  TableHead,
+  TextField,
   TableRow,
-  makeStyles
+  Toolbar,
+  makeStyles,
+  Typography
 } from "@material-ui/core";
-
-import { useTable } from "react-table";
+import ChevronLeftIcon from "mdi-material-ui/ChevronLeft";
+import ChevronRightIcon from "mdi-material-ui/ChevronRight";
+import Button from "../Button";
+import Select from "../Select";
 
 const useStyles = makeStyles((theme) => ({
+  pagination: {
+    borderBottom: "none",
+    color: theme.palette.colors.coolGrey500,
+    letterSpacing: 0.28,
+    paddingTop: theme.spacing(2)
+  },
   tableBody: {
     "& tr:nth-child(odd)": {
       backgroundColor: theme.palette.colors.black02
     }
   },
   tableHead: {
-    "& tr th": {
-      borderBottom: "none",
-      fontWeight: theme.typography.fontWeightSemiBold,
-      letterSpacing: 0.5,
-      padding: 0,
-      color: theme.palette.colors.coolGrey500
-    },
-    "& tr th:first-child": {
-      padding: "7px 0 1px 4px"
-    }
-  },
-  pagination: {
     borderBottom: "none",
-    color: theme.palette.colors.coolGrey500,
+    fontWeight: theme.typography.fontWeightSemiBold,
+    letterSpacing: 0.5,
+    padding: theme.spacing(0.5, 2),
+    color: theme.palette.colors.coolGrey500
+  },
+  tableCell: {
+    borderBottom: "none",
     letterSpacing: 0.28,
-    paddingTop: theme.spacing(2)
+    color: theme.palette.colors.coolGrey500
   }
 }));
 
@@ -43,38 +50,115 @@ const useStyles = makeStyles((theme) => ({
  * @returns {React.Component} A React component
  */
 const DataTable = React.forwardRef(function DataTable(props, ref) {
-  const { columns, data } = props;
+  const { columns, data, pageSizes } = props;
   const classes = useStyles();
 
   const {
     getTableProps,
     headerGroups,
-    rows,
-    prepareRow
-  } = useTable({ columns, data });
+    page,
+    prepareRow,
+    canPreviousPage,
+    canNextPage,
+    pageOptions,
+    pageCount,
+    gotoPage,
+    nextPage,
+    previousPage,
+    setPageSize,
+    state: [{ pageIndex, pageSize }]
+  } = useTable(
+    {
+      columns,
+      data
+    },
+    usePagination
+  );
 
   return (
-    <Table ref={ref} {...getTableProps()}>
-      <TableHead className={classes.tableHead}>
-        {headerGroups.map((headerGroup) => (
-          <TableRow {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map((column) => (
-              <TableCell {...column.getHeaderProps()}>{column.render("Header")}</TableCell>
-            ))}
-          </TableRow>
-        ))}
-      </TableHead>
-      <TableBody className={classes.tableBody}>
-        {rows.map((row) =>
-          prepareRow(row) || (
-            <TableRow {...row.getRowProps()}>
-              {row.cells.map((cell) => (
-                <TableCell {...cell.getCellProps()}>{cell.render("Cell")}</TableCell>
+    <div>
+      <Table ref={ref} {...getTableProps()}>
+        <TableHead className={classes.tableHead}>
+          {headerGroups.map((headerGroup) => (
+            <TableRow {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <TableCell
+                  classes={{
+                    root: classes.tableHead
+                  }}
+                  {...column.getHeaderProps()}
+                >
+                  {column.render("Header")}
+                </TableCell>
               ))}
             </TableRow>
           ))}
-      </TableBody>
-    </Table>
+        </TableHead>
+        <TableBody className={classes.tableBody}>
+          {page.map((row) =>
+            prepareRow(row) || (
+              <TableRow {...row.getRowProps()}>
+                {row.cells.map((cell) => (
+                  <TableCell
+                    classes={{
+                      root: classes.tableCell
+                    }}
+                    {...cell.getCellProps()}
+                  >
+                    {cell.render("Cell")}
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))}
+        </TableBody>
+      </Table>
+      <Toolbar>
+        <Box
+          display="flex"
+          alignItems="center"
+          paddingRight={2}
+        >
+          {"Page"}
+          <Box maxWidth={80} paddingLeft={1} paddingRight={1}>
+            <TextField
+              className={classes.pageInput}
+              margin="dense"
+              variant="outlined"
+              disableUnderline
+              type="number"
+              size="small"
+              min={1}
+              max={pageOptions.length}
+              defaultValue={pageIndex + 1}
+              value={pageIndex + 1}
+              onChange={(event) => {
+                let pageNumber = Number(event.target.value);
+                pageNumber = pageNumber > 0 ? pageNumber - 1 : 0;
+                gotoPage(pageNumber);
+              }}
+            />
+          </Box>
+          <span>{"of "}{pageCount}</span>
+        </Box>
+        <Box flex={1} maxWidth={120}>
+          <Select
+            value={{ label: `${pageSize} rows`, value: pageSize }}
+            onChange={(event) => {
+              setPageSize(Number(event.target.value));
+            }}
+            options={pageSizes.map((value) => ({ label: value, value }))}
+          />
+        </Box>
+        <Box flex={1} />
+        <Button onClick={() => previousPage()} disabled={!canPreviousPage}>
+          <ChevronLeftIcon /> Previous
+        </Button>
+        <Typography component="span">{" | "}</Typography>
+        <Button onClick={() => nextPage()} disabled={!canNextPage}>
+          Next <ChevronRightIcon />
+        </Button>
+      </Toolbar>
+    </div>
   );
 });
 
@@ -94,7 +178,15 @@ DataTable.propTypes = {
   /**
    * Row data as an array of objects
    */
-  data: PropTypes.arrayOf(PropTypes.object)
+  data: PropTypes.arrayOf(PropTypes.object),
+  /**
+   * Row data as an array of objects
+   */
+  pageSizes: PropTypes.arrayOf(PropTypes.number)
+};
+
+DataTable.defaultProps = {
+  pageSizes: [10, 20, 30, 40, 50]
 };
 
 export default DataTable;
