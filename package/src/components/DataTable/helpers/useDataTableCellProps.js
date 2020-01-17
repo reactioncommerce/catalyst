d154594bab0d104f71ed1b3bb269eff712d8d5d3
Mike Murray <hellomikemurray@gmail.com>
2020-01-16 19:27:54 -0800

    feat: update hook
    Signed-off-by: Mike Murray <hellomikemurray@gmail.com>

--- package/src/components/DataTable/helpers/useDataTableCellProps.js
+++ package/src/components/DataTable/helpers/useDataTableCellProps.js
@@ -1,61 +1,47 @@
-import PropTypes from "prop-types";
-
-const propTypes = {
-  columns: PropTypes.arrayOf(PropTypes.shape({
-    cellProps: PropTypes.shape({
-      isClickDisabled: PropTypes.bool
-    })
-  }))
-};
-
 /**
- * useMain
+ * useDataTableCellProps
  * @summary Hook used to add custom props to cells, and headers from the column definition
- * @param {Object} instance React Table instance
- * @returns {Object} React Table instance
+ * @param {Object} hooks React Table hook object
+ * @returns {Undefined} No return value
  */
-function useMain(instance) {
-  PropTypes.checkPropTypes(propTypes, instance, "property", "useDataTableCellProps");
-
-  const {
-    hooks: {
-      getHeaderProps,
-      getCellProps
-    }
-  } = instance;
-
+export default function useDataTableCellProps(hooks) {
   // Add a helpers function to get additional headerProps from the column definition
-  getHeaderProps.push((header) => {
-    if (typeof header.headerProps === "function") {
-      return header.headerProps(header, instance);
+  hooks.getHeaderProps.push((props, { column }) => {
+    if (typeof column.headerProps === "function") {
+      return [
+        props,
+        {
+          ...column.headerProps(column)
+        }
+      ];
     }
 
-    return {
-      ...header.headerProps
-    };
+    return [
+      props,
+      {
+        ...column.headerProps
+      }
+    ];
   });
 
   // Add a helpers function to get additional cellProps from the column definition
-  getCellProps.push((cell) => {
+  hooks.getCellProps.push((props, { cell }) => {
     if (typeof cell.column.cellProps === "function") {
-      return cell.column.cellProps(cell, instance);
+      return [
+        props,
+        {
+          ...cell.column.cellProps(cell)
+        }
+      ];
     }
 
-    return {
-      ...cell.column.cellProps
-    };
+    return [
+      props,
+      {
+        ...cell.column.cellProps
+      }
+    ];
   });
-
-  return instance;
-}
-
-/**
- * useDataTableCellProps
- * @param {Object} hooks Hooks object from react-table useTable
- * @returns {undefined} no return
- */
-export default function useDataTableCellProps(hooks) {
-  hooks.useMain.push(useMain);
 }
 
 useDataTableCellProps.pluginName = "useDataTableCellProps";
