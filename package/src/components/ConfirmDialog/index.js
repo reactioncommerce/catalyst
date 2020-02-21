faa51c7810622b644ba4724a618e55c8dda38a37
Mike Murray <hellomikemurray@gmail.com>
2020-02-21 14:11:34 -0800

    feat: export confirm dialog helpers
    Signed-off-by: Mike Murray <hellomikemurray@gmail.com>

--- package/src/components/ConfirmDialog/index.js
+++ package/src/components/ConfirmDialog/index.js
@@ -1,1 +1,4 @@
 export { default } from "./ConfirmDialog";
+export { default as ConfirmDialogBase } from "./helpers/ConfirmDialogBase";
+export { default as useConfirmDialog } from "./helpers/useConfirmDialog";
+
