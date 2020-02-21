6b39f4e3f766c4fc3283b036d6879417f8ddb409
Mike Murray <hellomikemurray@gmail.com>
2020-02-21 14:44:55 -0800

    test: add test for useConfirmDialog hook
    Signed-off-by: Mike Murray <hellomikemurray@gmail.com>

--- package/src/components/ConfirmDialog/ConfirmDialog.test.js
+++ package/src/components/ConfirmDialog/ConfirmDialog.test.js
@@ -2,6 +2,7 @@ import React from "react";
 import { render, fireEvent } from "../../tests/";
 import Button from "../Button";
 import ConfirmDialog from "./ConfirmDialog";
+import useConfirmDialog from "./helpers/useConfirmDialog";
 
 test("basic snapshot - with opening the dialog", () => {
   /* eslint-disable function-paren-newline */
@@ -14,6 +15,35 @@ test("basic snapshot - with opening the dialog", () => {
         <Button color="primary" onClick={openDialog} variant="contained">Open Confirm Dialog</Button>
       )}
     </ConfirmDialog>);
+  fireEvent.click(getByText("Open Confirm Dialog"));
+  expect(getByRole("dialog")).toBeInTheDocument();
+  expect(getByRole("dialog")).toHaveTextContent("Are you sure you want to do that?");
+  expect(getByRole("dialog")).toHaveTextContent("Are you sure?");
+  expect(getByRole("dialog")).toHaveTextContent("OK");
+  expect(getByRole("dialog")).toHaveTextContent("Cancel");
+  expect(getByRole("dialog")).toMatchSnapshot();
+  expect(asFragment()).toMatchSnapshot();
+});
+
+
+test("basic snapshot - with opening the dialog using the useConfirmDialog hook", () => {
+  // eslint-disable-next-line require-jsdoc
+  function TestComponent() {
+    const { openDialog, dialog } = useConfirmDialog({
+      title: "Are you sure?",
+      message: "Are you sure you want to do that?"
+    });
+
+    return (
+      <>
+        {dialog}
+        <Button color="primary" onClick={openDialog} variant="contained">Open Confirm Dialog</Button>
+      </>
+    );
+  }
+
+  const { asFragment, getByText, getByRole } = render(<TestComponent />);
+
   fireEvent.click(getByText("Open Confirm Dialog"));
   expect(getByRole("dialog")).toBeInTheDocument();
   expect(getByRole("dialog")).toHaveTextContent("Are you sure you want to do that?");
