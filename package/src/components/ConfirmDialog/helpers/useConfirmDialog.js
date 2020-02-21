9773cfbbf235908ffc36868051534b48d2615974
Mike Murray <hellomikemurray@gmail.com>
2020-02-21 14:11:09 -0800

    feat: add confirm dialog hook
    Signed-off-by: Mike Murray <hellomikemurray@gmail.com>

---
+++ package/src/components/ConfirmDialog/helpers/useConfirmDialog.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import ConfirmDialogBase from "./ConfirmDialogBase";
+
+/**
+ * @summary React hook for creating detached confirm dialogs with ease
+ * @param {Object} props Props to configure a confirm dialog
+ * @param {String} props.cancelActionText Cancel button text
+ * @param {Function} props.children Render prop `{({ openDialog }) => ()}`
+ * @param {Boolean} props.closeOnConfirm Close dialog on confirmation. Default `true`
+ * @param {String} props.confirmActionText Text for confirm button
+ * @param {ReactElement} props.content Child elements of the dialog. Use if this for rendering a custom components in the dialog.
+ * @param {Boolean} props.isOpen Dialog open/close state
+ * @param {String|React.Element} props.message Message body. May be a String or a React component. Use if your message is mostly text.
+ * @param {Function} props.onClose Close callback
+ * @param {Function} props.onConfirm Confirmation callback
+ * @param {String|React.Element} props.title Dialog title
+ * @returns {Object} An object containing {openDialog: func, dialog: React.Element}
+ */
+export default function useConfirmDialog({
+  closeOnConfirm = true,
+  onConfirm = () => {},
+  ...props
+}) {
+  const [isOpen, setOpen] = useState(false);
+
+  const openDialog = () => setOpen(true);
+  const closeDialog = () => setOpen(false);
+  const handleConfirm = () => {
+    onConfirm();
+
+    if (closeOnConfirm) {
+      closeDialog();
+    }
+  };
+
+  return {
+    isOpen,
+    openDialog,
+    closeDialog,
+    dialog: (
+      <ConfirmDialogBase
+        isOpen={isOpen}
+        onClose={closeDialog}
+        onConfirm={handleConfirm}
+        {...props}
+      />
+    )
+  };
+}
