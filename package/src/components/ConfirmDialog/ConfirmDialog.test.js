55529d751f1dd0e4dfd7947abcf60f5626de657c
Mike Murray <hellomikemurray@gmail.com>
2020-02-24 11:59:04 -0800

    test: additional content
    Signed-off-by: Mike Murray <hellomikemurray@gmail.com>

--- package/src/components/ConfirmDialog/ConfirmDialog.test.js
+++ package/src/components/ConfirmDialog/ConfirmDialog.test.js
@@ -53,3 +53,33 @@ test("basic snapshot - with opening the dialog using the useConfirmDialog hook",
   expect(getByRole("dialog")).toMatchSnapshot();
   expect(asFragment()).toMatchSnapshot();
 });
+
+test("basic snapshot - with opening the dialog using the useConfirmDialog hook with more content", () => {
+  // eslint-disable-next-line require-jsdoc, react/no-multi-comp
+  function TestComponent() {
+    const { openDialog, ConfirmDialog: ConfirmDialogComponent } = useConfirmDialog({
+      title: "Are you sure?",
+      content: <span>More content</span>,
+      message: "Are you sure you want to do that?"
+    });
+
+    return (
+      <>
+        <Button color="primary" onClick={openDialog} variant="contained">Open Confirm Dialog</Button>
+        <ConfirmDialogComponent />
+      </>
+    );
+  }
+
+  const { asFragment, getByText, getByRole } = render(<TestComponent />);
+
+  fireEvent.click(getByText("Open Confirm Dialog"));
+  expect(getByRole("dialog")).toBeInTheDocument();
+  expect(getByRole("dialog")).toHaveTextContent("Are you sure you want to do that?");
+  expect(getByRole("dialog")).toHaveTextContent("More content");
+  expect(getByRole("dialog")).toHaveTextContent("Are you sure?");
+  expect(getByRole("dialog")).toHaveTextContent("OK");
+  expect(getByRole("dialog")).toHaveTextContent("Cancel");
+  expect(getByRole("dialog")).toMatchSnapshot();
+  expect(asFragment()).toMatchSnapshot();
+});
